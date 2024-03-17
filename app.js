ac16a8070025a2c486b7e2547636dd06aa00b542
rudopulos <79414851+rudopulos@users.noreply.github.com>
2024-03-17 17:34:18 +0200

    hw03

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const express = require('express');
+const Joi = require('joi');
+const router = express.Router();
+const Contact = require('./routes/api/contacts');
 
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const validateContact = (contact) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+  });
+  return schema.validate(contact);
+};
 
 
-app.use(function(req, res, next) {
-  next(createError(404));
+router.patch('/:contactId/favorite', async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const { favorite } = req.body;
+
+    if (!favorite) {
+      return res.status(400).json({ message: 'missing field favorite' });
+    }
+
+    const existingContact = await Contact.findById(contactId);
+    if (!existingContact) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+
+
+    existingContact.favorite = favorite;
+    await existingContact.save();
+
+
+    return res.status(200).json(existingContact);
+  } catch (err) {
+    next(err);
+  }
 });
 
-
-app.use(function(err, req, res, next) {
-  
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
- 
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = router;
